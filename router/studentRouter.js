37f2053e071a2d1d57ad100484bee5130b50b141
Ravi Dharigond <66740454+Ravidharigond1997@users.noreply.github.com>
2024-01-12 18:20:25 +0530

    new changes is done

--- router/studentRouter.js
+++ router/studentRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verfiyUser } from "../middleware/authMiddleware.js";
-import { getProgress } from "../controller.js/studentController.js";
+import {
+  getAdmissionData,
+  getProgress,
+} from "../controller.js/studentController.js";
 
 const router = express.Router();
 
 router.get("/getProgressData/:rollNo", verfiyUser, getProgress);
+router.get("/getAdmissionData/:rollNo", verfiyUser, getAdmissionData);
 
 export default router;
